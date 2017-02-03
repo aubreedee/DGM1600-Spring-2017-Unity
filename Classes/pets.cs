7114b8a8de3e6527e425160f07307e66874f0394
Unknown <aubreedee@gmail.com>
2017-02-02 18:40:31 -0700

    In class assignment-Classes

---
+++ Classes/pets.cs
@@ -0,0 +1,29 @@
+// Class Example
+classes organize particular groupings of code. 
+it is the container for a specific set of code. 
+the example below is for animals.
+
+
+public class classes: MonoBehavior
+{
+    int score = 0;
+
+public class cats {
+   public int shortHair = 1;
+   public int longHair = 2;
+   public int tabby = 1;
+}
+
+public class dogs {
+    public int ankleBiters = 4;
+    public int mediumDog = 3;
+    public int massive = 1;
+}
+
+public class birds {
+    public int pigeon = 200;
+    public int seagull = 100;
+    public int hawk = 10;
+}
+}
+
---
+++ classes.cs
@@ -0,0 +1,25 @@
+// Class Example
+
+public class classes: MonoBehavior
+{
+    int score = 0;
+
+public class cats {
+   public int shortHair = 1;
+   public int longHair = 2;
+   public int tabby = 1;
+}
+
+public class dogs {
+    public int ankleBiters = 4;
+    public int mediumDog = 3;
+    public int massive = 1;
+}
+
+public class birds {
+    public int pigeon = 200;
+    public int seagull = 100;
+    public int hawk = 10;
+}
+}
+
