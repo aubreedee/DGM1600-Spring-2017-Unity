36f198ba27747f4b483baa0327639d44c7e53af0
Unknown <aubreedee@gmail.com>
2017-02-16 19:39:58 -0700

    Loop Examples

---
+++ Unity/DGM 1600 Spring 2017/Assets/for_loop.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class for_loop : MonoBehaviour {
+	public int bottles = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		for(int i=99; i>bottles; i--){
+			print(i+"bottles of apple beer on the wall");
+		}	
+	}
+}
---
+++ Unity/DGM 1600 Spring 2017/Assets/while_loop.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class while_loop : MonoBehaviour {
+	public int ammo = 0;
+
+	// Use this for initialization
+	void Start () {
+		while(ammo < 12){
+			ammo ++;
+			print("loading round" + ammo);
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
