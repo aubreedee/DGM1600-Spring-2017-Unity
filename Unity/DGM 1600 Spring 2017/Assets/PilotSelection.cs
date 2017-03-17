e05f1d16116f9dc10a16a6b98e83d51af23e91cc
Unknown <aubreedee@gmail.com>
2017-03-16 19:28:59 -0600

    Created Inventory Scene
    Created inventory scene and worked on PilotSelection Script...still need to populate the array for pilot selection

---
+++ Unity/DGM 1600 Spring 2017/Assets/PilotSelection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PilotSelection : MonoBehaviour {
+
+	public string [] pilot;
+
+	public void LoadPilot (string pilot);
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
