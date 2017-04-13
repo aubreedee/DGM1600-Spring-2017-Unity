88d6524d1222368cb75a789c182bf553a7455062
Unknown <aubreedee@gmail.com>
2017-04-13 10:25:08 -0600

    Merged School work with Home computer
    Its been a while since I have worked at home, many many changes

--- Unity/DGM 1600 Spring 2017/Assets/PilotSelection.cs
+++ Unity/DGM 1600 Spring 2017/Assets/PilotSelection.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using UnityEngine;
 
 public class PilotSelection : MonoBehaviour {
@@ -11,6 +12,25 @@ public class PilotSelection : MonoBehaviour {
 	
 	
 
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+    public GameObject[] pilot = { "Maverick", "Goose", "Faction", "Batmom", "Kitty", "Angel" };
+
+
+	// Use this for initialization
+	void Start () {
+
+        pilot = GameObject.FindGameObjectWithTag("pilot");
+
+        for(int )
+	}
+	
+>>>>>>> Stashed changes
 	// Update is called once per frame
 	void Update () {
 		
